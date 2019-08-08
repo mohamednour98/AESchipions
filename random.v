59986cf40fd1eeb9da8e838c3fc8915429008818
mohamednour98 <44243663+mohamednour98@users.noreply.github.com>
2019-08-08 19:33:34 +0200

    Update random.v

--- random.v
+++ random.v
@@ -1,19 +1,14 @@
+
 module nonce(
-  input  clk,
-  input  reset,
+  input [63:0]counter,
   output[63:0] outData
 );
 
   reg[63:0] data;
   assign outData = data[63:0];
-  wire feedback = data[63] ^ data[9] ^ data[7] ^ data[5] ^ data[3] ^ data[0];
-
-  always @(posedge clk or negedge reset) begin
-    if (~reset) 
-      data <= 64'hffffffffffffffff;
-    else begin
-      data <= {data[62:0], feedback};
-    end
-  end
-
+initial 
+begin 
+data<={8{8'haa}}^counter;
+end 
+assign outData = data[63:0];
 endmodule
