cd5b3b23bbbb1ff71f4e5de9846d2b7db42d3404
mohamednour98 <44243663+mohamednour98@users.noreply.github.com>
2019-08-08 19:51:07 +0200

    Update random.v

--- random.v
+++ random.v
@@ -1,14 +1,21 @@
 
 module nonce(
+input clk,
+input reset,
   input [63:0]counter,
   output[63:0] outData
 );
 
   reg[63:0] data;
   assign outData = data[63:0];
-initial 
+always@(posedge clk)
+begin 
+if(!reset)
 begin 
 data<={8{8'haa}}^counter;
+end
+else 
+data<=data;
 end 
 assign outData = data[63:0];
 endmodule
