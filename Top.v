3a957609f9367a669c93feb82db424b76e59860b
Osama Adam <osamaadam98@gmail.com>
2019-08-10 22:04:07 +0200

    Indentation and style fixes

--- Top.v
+++ Top.v
@@ -1,89 +1,83 @@
 module Top(
-  input next,
-  input clk,
-  input init,
-  input reset,
-  input[127:0] data,
-  input[127:0] key,
-  output[127:0] outData,
-  output ready
+  input wire next,
+  input wire clk,
+  input wire init,
+  input wire reset,
+  input wire[127:0] data,
+  input wire[127:0] key,
+  output wire[127:0] outData,
+  output wire ready
 );
-  localparam CTRL_IDLE  = 2'h0;
-  localparam CTRL_INIT  = 2'h1;
-  localparam CTRL_NEXT  = 2'h2;
+
+  localparam ctrlIdle  = 2'h0;
+  localparam ctrlInit  = 2'h1;
+  localparam ctrlNext  = 2'h2;
 
 
-  wire[127:0] roundKey, block, outEnc;
-  wire[31:0] beforeSub, afterSub;
-  wire[3:0] round;
   wire[63:0] count;
-  wire [63:0] nonce;
+  wire[63:0] nonce;
 
-  wire [127 : 0] round_key;
-  wire           key_ready;
+  wire[127 : 0] roundKey;
+  wire          keyReady;
 
 
-  wire [3 : 0]   enc_round_nr;
-  wire [127 : 0] enc_new_block;
-  wire           enc_ready;
-  wire [31 : 0]  enc_sboxw;
+  wire[3 : 0]   encRoundNum;
+  wire[127 : 0] encNewBlock;
+  wire          encReady;
+  wire[31 : 0]  encSBoxRequest;
 
  
-  wire [3 : 0]   dec_round_nr;
-  wire [127 : 0] dec_new_block;
-  wire           dec_ready;
-  wire [127 : 0] nonceIv;
-  wire [31 : 0]  keymem_sboxw;
-  wire [31 : 0]  new_sboxw;
+  wire[127 : 0] nonceIv;
+  wire[31 : 0]  keySBoxRequest;
+  wire[31 : 0]  sBoxResponse;
 
-  reg            enc_next;
-  reg [31 : 0]   muxed_sboxw;	
-  reg            dec_next;
-  reg            init_state;
-  reg [127 : 0]  muxed_new_block;
-  reg [3 : 0]    muxed_round_nr;
-  reg            muxed_ready;
-  reg [1 : 0] aes_core_ctrl_reg;
-  reg [1 : 0] aes_core_ctrl_new;
-  reg         aes_core_ctrl_we;
+  reg           encNext;
+  reg[31 : 0]   selSBoxRequest;	
+  reg           initState;
+  reg[127 : 0]  selNewBlock;
+  reg[3 : 0]    selRoundNum;
+  reg           selReady;
+  reg[1 : 0] ctrlReg;
+  reg[1 : 0] ctrlNew;
+  reg        ctrlWE;
 
-  reg         ready_reg;
-  reg         ready_new;
-  reg         ready_we;
-  reg  [127:0]result_out;
-  assign ready        = ready_reg;
-  assign result       = muxed_new_block;
-  assign outData      = result_out;
-  assign nonceIv={nonce[63:0],count[63:0]};
+  reg        readyReg;
+  reg        readyNew;
+  reg        readyWE;
+  reg [127:0]resultOut;
+  assign ready        = readyReg;
+  assign result       = selNewBlock;
+  assign outData      = resultOut;
+  assign nonceIv={nonce[63:0], count[63:0]};
 
-  aes_key_mem KeyGen(
+  KeyGen keyGenInst(
     .clk(clk),
-    .reset_n(reset),
+    .reset(reset),
     .key(key),
     .init(init),
-    .round(muxed_round_nr),
-    .round_key(round_key),
-    .ready(key_ready),
-    .sboxw(keymem_sboxw),
-    .new_sboxw(new_sboxw)
+    .round(selRoundNum),
+    .roundKey(roundKey),
+    .ready(keyReady),
+    .sBoxRequest(keySBoxRequest),
+    .sBoxResponse(sBoxResponse)
   );
 
-  EncryptionBlock Encryptor(
+  EncryptionBlock encryptor(
     .clk(clk),
-    .reset_n(reset),
-    .next(enc_next),
-    .round(enc_round_nr),
-    .round_key(round_key),
-    .sboxw(enc_sboxw),
-    .new_sboxw(new_sboxw),
+    .reset(reset),
+    .next(encNext),
+    .round(encRoundNum),
+    .roundKey(roundKey),
+    .sBoxRequest(encSBoxRequest),
+    .sBoxResponse(sBoxResponse),
     .block(nonceIv),
-    .new_block(enc_new_block),
-    .ready(enc_ready)
+    .newBlock(encNewBlock),
+    .ready(encReady)
   );
 
-  SubBox  sbox_inst(
-	.sboxw(muxed_sboxw), 
-	.new_sboxw(new_sboxw)
+  SubBox subBoxInst(
+	  .sBoxRequest(selSBoxRequest), 
+	  .sBoxResponse(sBoxResponse)
   );
 
   Counter counting(
@@ -91,138 +85,120 @@ module Top(
     .reset(reset),
     .count(count)
   );
-random fornonce(
-   .reset(reset),
-  .outData(nonce)
-);
+  random fornonce(
+    .reset(reset),
+    .outData(nonce)
+  );
 
   always @*
-    begin : sbox_mux
-      if (init_state)
+    begin : sBoxSel
+      if (initState)
         begin
-          muxed_sboxw = keymem_sboxw;
+          selSBoxRequest = keySBoxRequest;
         end
       else
         begin
-          muxed_sboxw = enc_sboxw;
+          selSBoxRequest = encSBoxRequest;
         end
-    end // sbox_mux
+    end
 
   always @*
-    begin : result_xor
-      if (enc_ready)
+    begin : resultXOR
+      if (encReady)
         begin
-          result_out = enc_new_block^data;
+          resultOut = encNewBlock ^ data;
         end
       else
         begin
-           result_out = 128'h0;
+           resultOut = 128'h0;
         end
-    end // result_xor
+    end 
 
   always @ (posedge clk or negedge reset)
-    begin: reg_update
+    begin: regUpdate
       if (!reset)
         begin
-         
-          ready_reg         <= 1'b1;
-          aes_core_ctrl_reg <= CTRL_IDLE;
+          readyReg         <= 1'b1;
+          ctrlReg <= ctrlIdle;
         end
       else
         begin
-         
-
-          if (ready_we)
-            ready_reg <= ready_new;
-
-          if (aes_core_ctrl_we)
-            aes_core_ctrl_reg <= aes_core_ctrl_new;
+          if (readyWE)
+            readyReg <= readyNew;
+          if (ctrlWE)
+            ctrlReg <= ctrlNew;
         end
-    end // reg_update
+    end 
   always @*
-    begin : encdec_mux
-      enc_next = 1'b0;
-      dec_next = 1'b0;
-
-      
-       
-          // Encipher operations
-          enc_next        = next;
-          muxed_round_nr  = enc_round_nr;
-          muxed_new_block = enc_new_block;
-          muxed_ready     = enc_ready;
-     
-    end // encdec_mux
+    begin 
+      encNext = 1'b0;
+      encNext        = next;
+      selRoundNum  = encRoundNum;
+      selNewBlock = encNewBlock;
+      selReady     = encReady;  
+    end 
 
     always @*
-    begin : aes_core_ctrl
-      init_state        = 1'b0;
-      ready_new         = 1'b0;
-      ready_we          = 1'b0;
-     
-     
-      aes_core_ctrl_new = CTRL_IDLE;
-      aes_core_ctrl_we  = 1'b0;
+    begin : ctrl
+      initState        = 1'b0;
+      readyNew         = 1'b0;
+      readyWE          = 1'b0;
 
-      case (aes_core_ctrl_reg)
-        CTRL_IDLE:
+      ctrlNew = ctrlIdle;
+      ctrlWE  = 1'b0;
+
+      case (ctrlReg)
+        ctrlIdle:
           begin
             if (init)
               begin
-                init_state        = 1'b1;
-                ready_new         = 1'b0;
-                ready_we          = 1'b1;
-            
-                
-                aes_core_ctrl_new = CTRL_INIT;
-                aes_core_ctrl_we  = 1'b1;
+                initState        = 1'b1;
+                readyNew         = 1'b0;
+                readyWE          = 1'b1;
+
+                ctrlNew = ctrlInit;
+                ctrlWE  = 1'b1;
               end
             else if (next)
               begin
-                init_state        = 1'b0;
-                ready_new         = 1'b0;
-                ready_we          = 1'b1;
+                initState        = 1'b0;
+                readyNew         = 1'b0;
+                readyWE          = 1'b1;
                
-                aes_core_ctrl_new = CTRL_NEXT;
-                aes_core_ctrl_we  = 1'b1;
+                ctrlNew = ctrlNext;
+                ctrlWE  = 1'b1;
               end
           end
 
-        CTRL_INIT:
+        ctrlInit:
           begin
-            init_state = 1'b1;
+            initState = 1'b1;
 
-            if (key_ready)
+            if (keyReady)
               begin
-                ready_new         = 1'b1;
-                ready_we          = 1'b1;
-                aes_core_ctrl_new = CTRL_IDLE;
-                aes_core_ctrl_we  = 1'b1;
+                readyNew         = 1'b1;
+                readyWE          = 1'b1;
+
+                ctrlNew = ctrlIdle;
+                ctrlWE  = 1'b1;
               end
           end
 
-        CTRL_NEXT:
+        ctrlNext:
           begin
-            init_state = 1'b0;
+            initState = 1'b0;
 
-            if (muxed_ready)
+            if (selReady)
               begin
-                ready_new         = 1'b1;
-                ready_we          = 1'b1;
+                readyNew         = 1'b1;
+                readyWE          = 1'b1;
                
-                aes_core_ctrl_new = CTRL_IDLE;
-                aes_core_ctrl_we  = 1'b1;
+                ctrlNew = ctrlIdle;
+                ctrlWE  = 1'b1;
              end
           end
 
-        default:
-          begin
-
-          end
-      endcase // case (aes_core_ctrl_reg)
-
-    end // aes_core_ctrl
-     
-
-   
+        default: begin end
+      endcase 
+    end
 endmodule
